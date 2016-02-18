0bf09cef7ec5de3f60ae659cd2a0974ed2bda023
Simon Kurka <dev@simon.kurka.cc>
2016-02-18 23:22:14 +0100

    added net buffer sizes

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,21 +1,23 @@
 class router::config {
 
-  file_line { 'backbone_sysctl_forwarding_ipv4':
-    ensure => present,
-    path   => '/etc/sysctl.conf',
-    line   => 'net.ipv4.ip_forward=1',
-  }
-
-  file_line { 'backbone_sysctl_forwarding_ipv6_all':
-    ensure => present,
-    path   => '/etc/sysctl.conf',
-    line   => 'net.ipv6.conf.all.forwarding=1',
-  }
-
-  file_line { 'backbone_sysctl_forwarding_ipv6_all':
-    ensure => present,
-    path   => '/etc/sysctl.conf',
-    line   => 'net.ipv6.conf.default.forwarding=1',
+  file_line {
+    default:
+      ensure => present,
+      path   => '/etc/sysctl.conf';
+    'router-net.ipv4.ip_forward':
+      line   => 'net.ipv4.ip_forward=1';
+    'router-net.ipv6.conf.all.forwarding':
+      line   => 'net.ipv6.conf.all.forwarding=1';
+    'router-net.ipv6.conf.default.forwarding':
+      line   => 'net.ipv6.conf.default.forwarding=1';
+    'router-net.core.rmem_max':
+      line   => 'net.core.rmem_max=83886080';
+    'router-net.core.wmem_max':
+      line   => 'net.core.wmem_max=83886080';
+    'router-net.core.rmem_default':
+      line   => 'net.core.rmem_max=83886080';
+    'router-net.core.wmem_default':
+      line   => 'net.core.wmem_max=83886080';
   }
 
 }
