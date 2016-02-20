7b5de91aa24fdd7a87b45401ce8c444496965c37
Simon Kurka <dev@simon.kurka.cc>
2016-02-20 12:42:57 +0100

    match added

--- manifests/config.pp
+++ manifests/config.pp
@@ -5,19 +5,26 @@ class router::config {
       ensure => present,
       path   => '/etc/sysctl.conf';
     'router-net.ipv4.ip_forward':
-      line   => 'net.ipv4.ip_forward=1';
+      line   => 'net.ipv4.ip_forward=1',
+      match  => '^net.ipv4.ip_forward=';
     'router-net.ipv6.conf.all.forwarding':
-      line   => 'net.ipv6.conf.all.forwarding=1';
+      line   => 'net.ipv6.conf.all.forwarding=1',
+      match  => '^net.ipv6.conf.all.forwarding=';
     'router-net.ipv6.conf.default.forwarding':
-      line   => 'net.ipv6.conf.default.forwarding=1';
+      line   => 'net.ipv6.conf.default.forwarding=1',
+      match  => '^net.ipv6.conf.default.forwarding=';
     'router-net.core.rmem_max':
-      line   => 'net.core.rmem_max=83886080';
+      line   => 'net.core.rmem_max=83886080',
+      match  => '^net.core.rmem_max=';
     'router-net.core.wmem_max':
-      line   => 'net.core.wmem_max=83886080';
+      line   => 'net.core.wmem_max=83886080',
+      match  => '^net.core.wmem_max=';
     'router-net.core.rmem_default':
-      line   => 'net.core.rmem_max=83886080';
+      line   => 'net.core.rmem_default=83886080',
+      match  => '^net.core.rmem_default=';
     'router-net.core.wmem_default':
-      line   => 'net.core.wmem_max=83886080';
+      line   => 'net.core.wmem_default=83886080',
+      match  => '^net.core.wmem_default=';
   }
 
 }
